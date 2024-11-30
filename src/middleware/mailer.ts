8921d472bd23f782ade6c8dfb9c86f2099fd48d5
yube01 <yubraja008@gmail.com>
2024-11-30 14:53:51 +0545

    nodemailer fix

--- src/middleware/mailer.ts
+++ src/middleware/mailer.ts
@@ -1,29 +1,36 @@
-import nodemailer, { Transporter } from 'nodemailer';
-import "dotenv/config"
+import nodemailer, { Transporter } from "nodemailer";
+import "dotenv/config";
 
 // Create a transporter configuration
 const transporter: Transporter = nodemailer.createTransport({
-  service: 'gmail',                  
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,  // Replace with your email user
-    pass: process.env.EMAIL_PASS   // Replace with your email password
+    user: process.env.EMAIL_USER, // Replace with your email user
+    pass: process.env.EMAIL_PASS, // Replace with your email password
   },
-  tls: { rejectUnauthorized: false }
+  tls: { rejectUnauthorized: true },
 });
 
 // Function to send an email
-export const sendMail = async (to: string, subject: string, text: string, html?: string): Promise<void> => {
+export const sendMail = async (
+  to: string,
+  subject: string,
+  text: string,
+  html?: string
+): Promise<void> => {
   try {
     await transporter.sendMail({
       from: process.env.EMAIL_USER, // sender address
-      to,                                            // receiver address
-      subject,                                       // Subject line
-      text,                                          // Plain text body
-      html                                           // HTML body (optional)
+      to, // receiver address
+      subject, // Subject line
+      text, // Plain text body
+      html, // HTML body (optional)
     });
-    console.log('Email sent successfully');
+    console.log("Email sent successfully");
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error("Error sending email:", error);
     throw error;
   }
 };
